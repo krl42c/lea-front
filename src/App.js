eb9a92ba776f566d56b22a49df74418344c44fa1
Karol Ciechlecki <karolc9991@gmail.com>
2020-11-01 14:40:48 +0100

    Updated app.js with new components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,39 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { Users } from './Users';
 import { Heading } from './Heading';
+import { Process } from './Process';
+import { BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
 
 function App() {
+    var isVisible = false;
     return(
+      <Router>
      <div >
-        <Heading/>
-        <Users/>
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 bg-white rounded">
+                    <h3 class="ml-1 mr-2">Linux Easy Admin</h3>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <Link to="/" class="nav-link"> Inicio </Link>
+                    </li>
+                    <li class="nav-item">
+                      <Link to="/users" class="nav-link"> Usuarios </Link>
+                    </li>
+                    <li class="nav-tem">
+                      <Link to="/process" class="nav-link">Procesos</Link>
+                    </li>
+                    </ul>
+                </div>
+                </nav>
+            </div>
+            <Route path="/users" component={Users} />
+            <Route path="/process" component={Process} />
       </div>
+      </Router>
     )
 }
 
