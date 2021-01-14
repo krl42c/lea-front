836f665606b0a147d84fb1aff73d052e5cf32751
Karol Ciechlecki <karolc9991@gmail.com>
2021-01-14 20:10:38 +0100

    Process stopping

--- src/components/Process.js
+++ src/components/Process.js
@@ -7,7 +7,15 @@ export class Process extends React.Component {
             processList : []
         };
     }
-
+  tick() {
+    fetch("/api/process")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+             processList : json
+        })
+      });
+  }
     componentDidMount() {
         fetch("/api/process")
           .then(res => res.json())
@@ -16,7 +24,27 @@ export class Process extends React.Component {
                 processList : json
             });
           });
+          this.interval = setInterval(() => this.tick(), 2000);
       }
+
+   componentWillUnmount() {
+    clearInterval(this.interval);
+    }
+
+    stopProcess(processName) {
+		return function() {
+            fetch("/api/stop_process", {
+                method:"POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: processName
+                })
+            })
+		}
+    }
 
     render() {
         return(
@@ -24,16 +52,14 @@ export class Process extends React.Component {
             <div>
             <ul class="list-group list-group-flush">
                    { this.state.processList.map(s => (  <li data-aos="fade-right" data-aos-duration="500" class="list-group-item bg-dark text-white"> {s}                         <a href="">
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                            </svg>
+
                         </a>
-                        <a class="ml-2" href="">
+                        <button class="btn btn-danger" href="" onClick={this.stopProcess({s})}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                           <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                         </svg>
-                        </a>
+                        </button>
                     <br/></li>))}
             </ul>
             </div>
