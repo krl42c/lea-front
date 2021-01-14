884eb75ceb511cebd17d261e333212c9c675f673
Karol Ciechlecki <karolc9991@gmail.com>
2021-01-14 22:51:02 +0100

    User deletion

--- src/components/Users.js
+++ src/components/Users.js
@@ -20,6 +20,18 @@ export class Users extends React.Component {
           });
       }
 
+	deleteUser(userName) {
+            fetch("/api/delete_user", {
+                method:"POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        username : userName
+                    })
+            });
+	}
     render() {
         return(
             //<div> {this.state.userList.map(s => (<li>{s}</li>))} </div>
@@ -32,7 +44,7 @@ export class Users extends React.Component {
 
                    { this.state.userList.map(s => (  <li class="list-group-item bg-dark text-white"> {s}
 
-                   <button class="btn btn-danger ml-2" href="" >
+                   <button class="btn btn-danger ml-2" href="" onClick={(event) => {this.deleteUser({s})}}>
                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
