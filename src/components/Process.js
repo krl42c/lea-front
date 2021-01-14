17d827457f3ca71cf00182770515b088b643b0ad
raul <raulmedinagn@gmail.com>
2021-01-14 22:42:08 +0100

    boton rojo eliminar que encima ni funciona

--- src/components/Process.js
+++ src/components/Process.js
@@ -51,9 +51,8 @@ export class Process extends React.Component {
             //<div> {this.state.userList.map(s => (<li>{s}</li>))} </div>
             <div>
             <ul class="list-group list-group-flush">
-                   { this.state.processList.map(s => (  <li class="list-group-item bg-dark text-white"> {s}                         <a href="">
+                   { this.state.processList.map(s => (  <li class="list-group-item bg-dark text-white"> {s}
 
-                        </a>
                         <button class="btn btn-danger" href="" onClick={this.stopProcess({s})}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
--- src/components/Users.js
+++ src/components/Users.js
@@ -30,7 +30,15 @@ export class Users extends React.Component {
                 </form>
             <ul class="list-group list-group-flush">
 
-                   { this.state.userList.map(s => (  <li class="list-group-item bg-dark text-white"> {s}</li>))}
+                   { this.state.userList.map(s => (  <li class="list-group-item bg-dark text-white"> {s}
+
+                   <button class="btn btn-danger ml-2" href="" >
+                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                   </svg>
+                   </button>
+                   </li>))}
             </ul>
             </div>
         )
